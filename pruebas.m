b9465dcccff72ffe2a79655bebf60101d296d135
megaelius <abad.elias00@gmail.com>
2020-05-01 10:50:05 +0200

    fillo da puta

--- pruebas.m
+++ pruebas.m
@@ -1,3 +1,3 @@
-tr_seed    = 230580;    % Seed of the training (id alex).
-te_seed    = 158096;    % Seed of the test (id elias).
+tr_seed    = 123456;    % Seed of the training (id alex).
+te_seed    = 789101;    % Seed of the test (id elias).
 uo_nn_batch(tr_seed,te_seed);
